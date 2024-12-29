225169351df297fb03bd052d2bd249ab5668690f
AngelmProoxD <96387072+AngelmProoxD@users.noreply.github.com>
2024-12-29 16:44:30 +0100

    Add files via upload

---
+++ device.mk
@@ -0,0 +1,39 @@
+LOCAL_PATH := device/lenovo/X306F
+
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+TARGET_OTA_ASSERT_DEVICE := X306F
+
+TARGET_COPY_OUT_VENDOR := vendor
+
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXTRA_LANGUAGES := true
+TW_INCLUDE_NTFS_3G := true
+TW_USE_TOOLBOX := true
+TW_INCLUDE_RESETPROP := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
+TW_DEFAULT_BRIGHTNESS := 800
+TARGET_USES_MKE2FS := true
+TW_NO_LEGACY_PROPS := true
+TW_USE_NEW_MINADBD := true
+TW_NO_BIND_SYSTEM := true
+TW_NO_SCREEN_BLANK := true
+TW_EXCLUDE_APEX := true
+TW_EXCLUDE_TWRPAPP := true
+TW_FRAMERATE := 60
+TW_DEVICE_VERSION := angelm
+TW_OVERRIDE_SYSTEM_PROPS := \
+    "ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental" 
+TW_SUPPORTS_DYNAMIC_PARTITIONS := true
+TW_INCLUDE_CRYPTO := true 
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
